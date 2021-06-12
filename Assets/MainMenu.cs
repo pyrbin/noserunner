f392c454ca7c75a5c50c79e85048bd87eb200382
mrlingon <gustav.alvinsson@telia.com>
2021-06-12 14:10:28 +0200

    Created a main menu with buttons
    its buttons

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ExitButton() {
+        Application.Quit();
+        Debug.Log("Game Terminated.");
+    }
+
+    public void StartButton() {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
