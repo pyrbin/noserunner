5d24bdb1ab243f152cc10b2e01f17621e19e9ee5
smultronbusken <fredholmisak@outlook.com>
2021-06-13 13:22:04 +0200

    Fixed camera and cursor while paused

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,37 +10,54 @@ public class PauseMenu : MonoBehaviour
 
     public GameObject pauseMenuUI;
 
+
+    public CinemachineVirtualCamera camera;
+
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape)){
-            if(GameIsPaused){
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
                 Resume();
             }
-            else {
+            else
+            {
                 Pause();
             }
         }
     }
 
-    public void Resume() {
+    public void Resume()
+    {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        camera.enabled = true;
     }
 
-    void Pause() {
+
+    void Pause()
+    {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        camera.enabled = false;
     }
 
-    public void LoadMenu() {
+    public void LoadMenu()
+    {
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
     }
 
-    public void QuitGame() {
+    public void QuitGame()
+    {
         Debug.Log("Quitting application...");
         Application.Quit();
     }
